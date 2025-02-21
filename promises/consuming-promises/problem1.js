25d0b9d60e147143c2546fbc59564694e0a95c80
AjayMarukonda22 <ajaymarukonda12@gmail.com>
2025-02-21 12:59:09 +0530

    update code with error handling

---
+++ promises/consuming-promises/problem1.js
@@ -0,0 +1,112 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+/*
+    Problem 1:
+    
+    Using promises and the fs module's asynchronous functions, do the following:
+        1. Create a directory of random JSON files
+        2. Delete those files simultaneously 
+*/
+
+// Consuming the Promises using .then()
+
+//create a directory
+const createDirectory = (dirPath) => {
+
+    return fs.mkdir(dirPath, {recursive: true})
+             .then(() => {
+                console.log('Created directory successfully');
+                return dirPath;
+             })
+             .catch((err) => {
+                console.error('Error while creating directory', err);
+                throw err;
+             })
+}
+
+//create random files
+const createRandomFiles = (dirPath, count) => {
+
+    const filePromises = [];
+    for(let i = 1 ; i <= count ; i++) {
+        let filePath = path.join(dirPath, `file${i}.json`);
+        let data = JSON.stringify({id : i, value: `file${i}`});
+
+        filePromises.push(fs.writeFile(filePath, data));
+    }
+
+    return Promise.all(filePromises)
+                  .then(() => {
+                    console.log('Created files successfully');
+                    return dirPath;
+                  })
+                  .catch((err) => {
+                    console.error("Error while creating files", err);
+                    throw err;
+                  })
+}
+
+//read the directory
+const readDirectory = (dirPath) => {
+    return fs.readdir(dirPath)
+             .then((files) => {
+                console.log("Read the files successfully");
+                return files;
+             })
+             .catch((err) => {
+                console.error('error while reading the directory', err);
+                throw err;
+             })
+}
+
+
+//delete the files
+const deleteFiles = (dirPath, files) => {
+
+    const deletePromises =  files.map((file) => {
+        let filePath = path.join(dirPath, file);
+        return fs.unlink(filePath);
+    });
+
+    return Promise.all(deletePromises)
+                  .then(() => {
+                    console.log("Deleted all the files successfully");
+                    return dirPath;
+                  })
+                  .catch((err) => {
+                    console.error('Error while deleting files');
+                    throw err;
+                  })
+}
+
+
+//delete the directory
+const deleteDirectory = (dirPath) => {
+    return fs.rmdir(dirPath)
+            .then(() => {
+                console.log("Deleted the directory successfully");
+            })
+            .catch((err) => {
+                console.error('Error while deleting the directory', err);
+            })
+}
+
+
+//testing
+const dirPath = path.join(__dirname, "randomFiles");
+//promises (consuming the readymade promises)
+createDirectory(dirPath)
+                        .then((dirPath) =>  createRandomFiles(dirPath, 5))
+                        .then((dirPath) =>  readDirectory(dirPath))
+                        .then((files)   =>  {
+                            return new Promise((resolve) => {
+                                setTimeout(() => { deleteFiles(dirPath, files).then(() => resolve(dirPath))}, 5000);
+                            });
+                        })
+                        .then((dirPath) => {
+                            return new Promise((resolve) => {
+                                setTimeout(() => { deleteDirectory(dirPath).then(resolve)}, 3000);
+                            });
+                        })
+                        .catch((err)    =>  console.error('Process failed:', err))
---
+++ promises/consuming-promises/problem2.js
@@ -0,0 +1,141 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+/*
+    Problem 2:
+    
+    Using promises and the fs module's asynchronous functions, do the following:
+        1. Read the given file lipsum.txt
+        2. Convert the content to uppercase & write to a new file. Store the name of the new file in filenames.txt
+        3. Read the new file and convert it to lower case. Then split the contents into sentences. Then write it to a new file. Store the name of the new file in filenames.txt
+        4. Read the new files, sort the content, write it out to a new file. Store the name of the new file in filenames.txt
+        5. Read the contents of filenames.txt and delete all the new files that are mentioned in that list simultaneously.
+*/
+
+//Read file
+const readFileAsync = (filePath) => {
+    return fs.readFile(filePath, 'utf8')
+            .then((content) => {
+                console.log(`Read the file: ${filePath}`);
+                return content;
+            })
+            .catch((err) => {
+                console.error(`Error while reading file ${filePath}`, err);
+                throw err;
+            })
+}
+
+//convert to uppercase and write to a file
+const toUppercaseAndWriteAsync = (content) => {
+           let upperCaseContent = content.toUpperCase();
+           let upperCaseFile = "upperCaseFile.txt";
+           let upperCaseFilePath = path.join(__dirname, upperCaseFile);
+
+           return fs.writeFile(upperCaseFilePath, upperCaseContent)
+                     .then(() => {
+                        console.log(`converted the content to uppercase and wrote to file ${upperCaseFile}`)
+                        return upperCaseFile;
+                     })
+                     .catch((err) => {
+                        console.error(`Error while writing file ${upperCaseFile}`, err);
+                        throw err;
+                     })
+}
+
+//append the content to a file
+const appendFileAsync = (fileNamesFilePath, data) => {
+    return fs.appendFile(fileNamesFilePath, data + '\n')
+            .then(() => {
+                console.log(`Append the ${data} into ${fileNamesFilePath}`);
+                return data;
+            })
+            .catch((err) => {
+                console.error(`Error while appending the file ${data}`);
+                throw err;
+            })
+}
+
+//convert to lowercase and write to a file
+const toLowerCaseAndWriteAsync = (content) =>  {
+  let lowerCaseContent = content.toLowerCase();
+  let lowerCaseFile = 'lowerCaseFile.txt';
+  let lowerCaseFilePath = path.join(__dirname, lowerCaseFile);
+
+  let sentences = lowerCaseContent.split('. ').map((sentence) => sentence.trim()).join('.\n');
+
+  return fs.writeFile(lowerCaseFilePath, sentences)
+           .then(() => {
+            console.log(`converted the content to lowercase and split it into sentences and wrote to a file ${lowerCaseFile}`);
+            return lowerCaseFile;
+           })
+           .catch((err) => {
+            console.error(`Error while writing the file ${lowerCaseFile}`, err);
+            throw err;
+           })
+} 
+
+//sort the content and write to a file
+const sortTheContentAndWriteAsync = (content) => {
+        let sortedContent = content.split('\n').sort().join('\n');
+        let sortedContentFile = 'sortedContentFile.txt';
+        let sortedContentFilePath = path.join(__dirname, sortedContentFile);
+
+        return fs.writeFile(sortedContentFilePath, sortedContent)
+                .then(() => {
+                    console.log(`Sorted the content and wrote to file ${sortedContentFile}`);
+                    return sortedContentFile;
+                })
+                .catch((err) => {
+                    console.error(`Error while writing the file ${sortedContentFile}`, err);
+                    throw err;
+                })
+}
+
+//delete the files
+const deleteFilesAsync = (content) => {
+    let filesArray = content.split('\n').filter(Boolean);
+    
+    let deletePromises =  filesArray.map((file) => {
+       let filePath = path.join(__dirname, file);
+       return fs.unlink(filePath);
+    });
+
+    return Promise.all(deletePromises)
+                  .then(() => {
+                    console.log("Deleted the files successfully");
+                    return ;
+                  })
+                  .catch((err) => {
+                    console.error(`Error while deleting files`, err)
+                    throw err;
+                  })
+ }
+
+//Promises (consuming readymade promises)
+const fileNamesFilePath = path.join(__dirname, 'filenames.txt');
+const lipsumFilePath = path.join('./', 'lipsum.txt');
+
+readFileAsync(lipsumFilePath)
+                       .then((content)  =>  toUppercaseAndWriteAsync(content))
+                       .then((fileName) =>  appendFileAsync(fileNamesFilePath, fileName))
+                       .then((fileName) =>  {
+                          let filePath = path.join(__dirname, fileName);
+                           return readFileAsync(filePath);
+                            })
+                        .then((content)  =>  toLowerCaseAndWriteAsync(content))
+                        .then((fileName) =>  appendFileAsync(fileNamesFilePath, fileName))
+                        .then((fileName) => {
+                  let filePath = path.join(__dirname, fileName);
+                      return readFileAsync(filePath);
+                            })
+                        .then((content)  =>  sortTheContentAndWriteAsync(content))
+                        .then((fileName) =>  appendFileAsync(fileNamesFilePath, fileName))
+                        .then(()         =>  readFileAsync(fileNamesFilePath))
+                       .then((contents)  => { 
+                        return new Promise((resolve) => {
+                            setTimeout(() => { deleteFilesAsync(contents).then(resolve) }, 10000);
+                              });
+                        })
+                       .catch((err) => {
+                        console.error(err);
+                          })
